abdb794fe9b7e12c01c7c0d14ce729f683824b55
solygambas <51904909+solygambas@users.noreply.github.com>
2023-04-27 16:13:45 +0200

    feat: add palette endpoint

--- 01-color-palette-generator/app.py
+++ 01-color-palette-generator/app.py
@@ -1,11 +1,56 @@
+from dotenv import dotenv_values
 from flask import Flask, render_template, request
+import json
+import openai
+
+config = dotenv_values(".env")
+openai.api_key = config["OPENAI_API_KEY"]
 
 app = Flask(__name__,
             template_folder='templates')
 
+def get_colors(query):
+    prompt = f"""
+    You are a color palette generating assistant that responds to text prompts for color palettes.
+    You should generate color palettes that fit the theme, mood, or instructions in the prompt.
+    The palettes should be between 2 and 8 colors.
+
+    Q: Convert the following verbal description of a color palette into a list of colors: The Meditation
+    A: ["#594F4F", "#A1887F", "#BCAAA4", "#81C784", "#455A64"]
+
+    Q: Convert the following verbal description of a color palette into a list of colors: The Mediterranean Sea
+    A: ["#1A237E", "#1565C0", "#1E88E5", "#90CAF9", "#E1F5FE"]
+
+    Desired Format: a JSON array of hexadecimal color codes
+
+    Q: Convert the following verbal description of a color palette into a list of colors: {query}
+    A:
+    """
+
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=prompt,
+    max_tokens=30
+    )
+
+    colors = json.loads(response["choices"][0]["text"])
+    return colors
+
+@app.route("/palette", methods=["POST"])
+def prompt_to_palette():
+    query = request.form.get("query")
+    colors = get_colors(query)
+    return { "colors": colors }
+
 @app.route("/")
 def index():
-    return "HELLO THERE FROM FLASK"
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="The dog says ",
+        max_tokens=10
+    )
+    return response["choices"][0]["text"]
+    # return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
