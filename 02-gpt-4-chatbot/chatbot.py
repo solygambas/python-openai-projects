f862fec9e32fb2e4dcdc16cdb346ccb8ff4f68dc
solygambas <51904909+solygambas@users.noreply.github.com>
2023-04-30 16:17:04 +0200

    feat: add persisting messages

--- 02-gpt-4-chatbot/chatbot.py
+++ 02-gpt-4-chatbot/chatbot.py
@@ -4,15 +4,19 @@ from dotenv import dotenv_values
 config = dotenv_values(".env")
 openai.api_key = config["OPENAI_API_KEY"]
 
+messages = []
+
 while True:
     try:
         user_input = input("You: ")
+        messages.append({"role": "user", "content": user_input})
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=[{"role": "user", "content": user_input}],
+            messages=messages,
             max_tokens=10,
         )
-        print(response["choices"][0].message.content)
+        messages.append(response["choices"][0].message.to_dict())
+        print("Assistant: ", response["choices"][0].message.content)
     except KeyboardInterrupt:
         print("Exiting...")
         break
